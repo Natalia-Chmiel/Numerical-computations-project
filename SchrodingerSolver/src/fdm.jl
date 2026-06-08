8e9811bb48095286893e35120ca77d656e1da8e4
Natalia Chmiel <277484@student.pwr.edu.pl>
2026-06-08 21:14:23 +0200

    optimizes solver fdm

--- SchrodingerSolver/src/fdm.jl
+++ SchrodingerSolver/src/fdm.jl
@@ -1,20 +1,16 @@
 using LinearAlgebra
 
 function solve_FDM(V::Vector{Float64}, x::Vector{Float64}, m::Float64, planck::Float64)
+
     N = length(x)
     dx = x[2] - x[1]
-    
-    H = zeros(N, N)
-    
-    for i in 1:N
-        H[i, i] = V[i] + planck^2 / (m * dx^2) 
-        if i > 1
-            H[i, i-1] = (-1) * planck^2 / (2 * m * dx^2) 
-        end
-        if i < N
-            H[i, i+1] = (-1) * planck^2 / (2 * m * dx^2)
-        end
-    end
+
+    C = planck^2 / (2.0 * m * dx^2) 
+
+    main_diag = V .+ (2.0 * C) 
+    under_diag = fill(-C, N - 1)
+
+    H = SymTridiagonal(main_diag, under_diag)
     
     eigenvalues, eigenvectors = eigen(H)
     
