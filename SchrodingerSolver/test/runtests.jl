9e028a1cd62ed7f8c47c58b06816767e0e77d5e5
Julia Zalewska <277433@student.pwr.edu.pl>
2026-06-15 21:17:08 +0200

    Test

--- SchrodingerSolver/test/runtests.jl
+++ SchrodingerSolver/test/runtests.jl
@@ -16,12 +16,10 @@ using SchrodingerSolver
         @test isapprox(energies_fdm[1], E_analytic_1, rtol=0.01)
     end
 
-    # ====================================================================
-    # Odkomentować, jak będzie gotowy solve_Numerov
-    # @testset "Test Metody Numerova" begin
-    #     energies_num, wavefuncs_num = solve_Numerov(V, x, m, planck)
-    #     
-    #     @test isapprox(energies_num[1], E_analytic_1, rtol=0.001)
-    # end
+    @testset "Test Metody Numerova" begin
+        energies_num, wavefuncs_num = solve_Numerov(V, x, m, planck)
+         
+        @test isapprox(energies_num[1], E_analytic_1, rtol=0.001)
+    end
 
 end
